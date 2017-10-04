6dfb7c3507678fc7bd9a76f1f9e2cd36fd2a36c2
AngelmoranT <morantvalls@hotmail.com>
2017-10-04 12:55:26 +0200

    Iniciada CCategoria( lectura de categoria y nuevo)

---
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+namespace CCategoria
+{
+    public partial class CategoriaView : Gtk.Window
+    {
+        public CategoriaView() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+
+            saveAction.Activated += delegate{
+                string nombre = entryNombre.Text;
+                IDbCommand dbComand = App.Instance.Connection.CreateCommand();
+                dbComand.CommandText = "insert into categoria (noombre) values (@nombre)";
+                IDataParameter dbDataParameter = dbComand.CreateParameter();
+                dbDataParameter.ParameterName = "nombre";
+                dbDataParameter.Value = nombre;
+                dbDataParameter.Value = nombre;
+                dbComand.Parameters.Add(dbDataParameter);
+                dbComand.ExecuteNonQuery();
+                Destroy();
+            };
+        }
+    }
+}
